bad617b9f0a23c32ec9188df86fe1c0a004c5d0f
Pierre <legrand.work@gmail.com>
2015-03-02 16:59:19 +0100

    Add Fog Region When upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,12 +3,13 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Configuration[:aws_access_key],
-      aws_secret_access_key: Configuration[:aws_secret_key]
+      aws_secret_access_key: Configuration[:aws_secret_key],
+      region: Configuration[:aws_region]
     }
     config.fog_directory  = Configuration[:aws_bucket]
     config.fog_attributes = { 'Cache-Control' => 'max-age=315576000' }  # optional, defaults to { }
     config.asset_host = Configuration[:asset_host]
-    config.fog_region = Configuration[:aws_region]
+    #config.fog_region = Configuration[:aws_region]
   else
     config.enable_processing = false if Rails.env.test? or Rails.env.cucumber?
   end
