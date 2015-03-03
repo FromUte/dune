f17a6580f0b0c799749e76ef3d4c3b8d06199dd4
Pierre <legrand.work@gmail.com>
2015-03-04 00:00:42 +0100

    Correction romain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,11 +32,13 @@ class ApplicationController < ActionController::Base
 
   # Redirect to the appropriate domain i.e. example.com
   def ensure_domain
+    unless Rails.env.development?
     domain_to_redirect_to = 'www.dune-investissement.fr'
     domain_exceptions = ['www.dune-investissement.fr','dune-investissement.herokuapp.com']
     should_redirect = !(domain_exceptions.include? request.host)
     new_url = "#{request.protocol}#{domain_to_redirect_to}#{request.fullpath if request.fullpath != '/'}"
     redirect_to new_url, status: :moved_permanently if should_redirect
+    end
   end
 
 end
